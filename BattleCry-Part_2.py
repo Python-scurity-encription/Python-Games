1d9b4fcd131211c32bfabe5e4e3bcb1f80613d56
Jason <70834549+Jason100129@users.noreply.github.com>
2020-09-12 21:19:41 +0800

    fix some grammar error

--- BattleCry-Part_2.py
+++ BattleCry-Part_2.py
@@ -1,8 +1,8 @@
 import time
-print('Second level, Lost Temple!').
-sleep(2)
+print('Second level, Lost Temple!')
+time.sleep(2)
 print('You found a temple in the jungle...')
-sleep(2)
+time.sleep(2)
 print('You walked in and see some angry bees!')
-sleep(3)
+time.sleep(3)
 print('System prompt: angry bees, HP: 2, speed: medium, attack power: 1, quantity: 20, distance 8 meters from you.')
