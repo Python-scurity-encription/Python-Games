c9d90ad783d34a55f191d5727e2fa5a9d6a9bb42
Jason <70834549+Jason100129@users.noreply.github.com>
2020-09-13 12:19:37 +0800

    Update BattleCry-Part_2.py

--- BattleCry-Part_2.py
+++ BattleCry-Part_2.py
@@ -6,3 +6,5 @@ time.sleep(2)
 print('You walked in and see some angry bees!')
 time.sleep(3)
 print('System prompt: angry bees, HP: 2, speed: medium, attack power: 1, quantity: 20, distance 8 meters from you.')
+time.sleep(4)
+
