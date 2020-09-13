288633705861d9f596e7ba98029bdeb7c520fd9d
Jason <70834549+Jason100129@users.noreply.github.com>
2020-09-13 15:23:10 +0800

    Update BattleCry-Part_2.py

--- BattleCry-Part_2.py
+++ BattleCry-Part_2.py
@@ -1,1 +1,10 @@
+import time
+print('Second level, Lost Temple!')
+time.sleep(2)
+print('You found a temple in the jungle...')
+time.sleep(2)
+print('You walked in and see some angry bees!')
+time.sleep(3)
+print('System prompt: angry bees, HP: 2, speed: medium, attack power: 1, quantity: 20, distance 8 meters from you.')
+time.sleep(3)
 
