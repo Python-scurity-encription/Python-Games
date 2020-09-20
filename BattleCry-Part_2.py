138df0ebc4ff13f3a58b5c791f24a514e71737ce
Lele1357 <67990770+Lele1357@users.noreply.github.com>
2020-09-20 22:02:43 +0800

    Update BattleCry-Part_2.py

--- BattleCry-Part_2.py
+++ BattleCry-Part_2.py
@@ -1,45 +1,49 @@
-import time
-print('Second level, Lost Temple!')
-time.sleep(2)
-print('You found a temple in the jungle...')
-time.sleep(2)
-print('You walked in and see some angry bees!')
-time.sleep(3)
-print('System prompt: angry bees, HP: 2, speed: medium, attack power: 1, quantity: 20, distance 8 meters from you.')
-time.sleep(3)
-print('System reward: cleave, attack power: 70, distance: 50 meters, use input c, blood volume: 50.')
-time.sleep(2)
+name=input('hello,Warrior, what\'s your name?')
+input('Hello '+name+' Warrior, now your information will be displayed...Please wait...')
+input('HP: 5, combat power: 10, hunger value: 0, normal attack distance: uncertain.')
+blood=5
+fight=10
+hungry=0
+input('Lost Temple!')
+input('You found a temple in the jungle...')
+
+input('You walked in and see some angry bees!')
+
+input('System prompt: angry bees, HP: 2, speed: medium, attack power: 1, quantity: 20, distance 8 meters from you.')
+
+input('System reward: cleave, attack power: 70, distance: 50 meters, use input c, blood volume: 50.')
+
 box=input('DOD')
 if box == 'c':
-    print('Excellent!')
-    time.sleep(1)
-    print('You cleaved them, and they are all die!')
+    input('Excellent!')
+    
+    input('You cleaved them, and they are all die!')
 else:
-    print('You only killed one bee, and the others killed you!')
+    input('You only killed one bee, and the others killed you!')
     quit()
-time.sleep(2)
-print('You go on and on...')
-time.sleep(2)
-print('You found a chest.')
-time.sleep(1)
+
+input('You go on and on...')
+
+input('You found a chest.')
+
 box=input('Open the chest and press o, do not open and press other.')
 if box == 'o':
-    print('You opened the chest,')
-    time.sleep(1)
-    print('And you find a iron sword!')
-    time.sleep(1)
-    print('System prompt: iron sword, increase attack damage 30, reduce attack power 10.')
-    time.sleep(2)
+    input('You opened the chest,')
+    
+    input('And you find a iron sword!')
+    
+    input('System prompt: iron sword, increase attack damage 30, reduce attack power 10.')
+    
 else:
-    print('You do not open the chest.')
-    time.sleep(2)
-print('You see a skeleton behind you!')
-time.sleep(2)
-print('System prompt: skeleton, HP: 50, speed: slow, attack power: 5, distance 5 meters from you.')
-time.sleep(3)
+    input('You do not open the chest.')
+    
+input('You see a skeleton behind you!')
+
+input('System prompt: skeleton, HP: 50, speed: slow, attack power: 5, distance 5 meters from you.')
+
 box=input('DOD')
 if box == 'd':
-    print('You attacked it, and it was die.')
+    input('You attacked it, and it was die.')
 else:
-    print('You used a trick, it died and gave you a bow.')
-print('System prompt: bow, increase attack range 10.')
+    input('You used a trick, it died and gave you a bow.')
+input('System prompt: bow, increase attack range 10.')
