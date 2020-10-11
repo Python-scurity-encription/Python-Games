bce8ed3ae6e7f8ccca82d5d301e3701e09fd468a
Lele1357 <67990770+Lele1357@users.noreply.github.com>
2020-10-11 21:26:41 +0800

    Update BattleCry-Jungle Adventure.py

--- BattleCry-Jungle Adventure.py
+++ BattleCry-Jungle Adventure.py
@@ -146,7 +146,7 @@ print('You found a little house beside the river.')
 time.sleep(2)
 box=input('Walked in and press w, do not walk in press anything else.')
 if box == 'w':
-      print('You walked in and saw a 
+      print('You walked in and saw a ...')
 print('System warning: the first level of the boss is coming!')
 time.sleep(2)
 print('A "mountain" appeared in front of you...')
