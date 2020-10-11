ec0ab8100cea573c0d87057bd14d97099aba2fa0
Jason <70834549+Jason100129@users.noreply.github.com>
2020-10-11 21:36:36 +0800

    Update BattleCry-Lost Temple.py

--- BattleCry-Lost Temple.py
+++ BattleCry-Lost Temple.py
@@ -6,42 +6,27 @@ fight=10
 hungry=0
 input('Welcome to the new Chapter, Lost Temple!')
 input('You found a temple in the jungle...')
-
 input('You walked in and see some angry bees!')
-
-input('System prompt: angry bees, HP: 2, speed: medium, attack power: 1, quantity: 20, distance 8 meters from you.')
-
+input('System prompt: angry bees, HP: 2, speed: middle, attack power: 1, quantity: 20, distance 8 meters from you.')
 input('System reward: cleave, attack power: 70, distance: 50 meters, use input c, blood volume: 50.')
-
 box=input('DOD')
-
 if box == 'c':
     input('Excellent!')
-    
     input('You cleaved them, and they are all die!')
 else:
     input('You only killed one bee, and the others killed you!')
     quit()
-
 input('You go on and on...')
-
 input('You found a chest.')
-
 box=input('Open the chest and press o, do not open and press other.')
 if box == 'o':
     input('You opened the chest,')
-    
     input('And you find a iron sword!')
-    
     input('System prompt: iron sword, increase attack damage 30, reduce attack power 10.')
-    
 else:
     input('You do not open the chest.')
-    
 input('You see a skeleton behind you!')
-
 input('System prompt: skeleton, HP: 50, speed: slow, attack power: 5, distance 5 meters from you.')
-
 box=input('DOD')
 if box == 'd':
     input('You attacked it, and it is die.')
